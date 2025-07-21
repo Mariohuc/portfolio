921bd512480734e64ccb3eef69765357a860f04a
Mariohuc <mhuaypuna@unsa.edu.pe>
2025-07-20 22:40:16 -0500

    Fix: UI improvements

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -43,7 +43,7 @@ const Contact = () => {
       <div role="status">
         <svg
           aria-hidden="true"
-          class="inline w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+          className="inline w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
@@ -57,7 +57,7 @@ const Contact = () => {
             fill="currentFill"
           />
         </svg>
-        <span class="sr-only">Loading...</span>
+        <span className="sr-only">Loading...</span>
       </div>
     );
   } else {
